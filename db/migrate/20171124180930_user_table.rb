826f3ba0ba9b2198132ccd91b4759a99ba33b9d6
Mars Howard <marsbhoward@gmail.com>
2017-11-24 18:17:47 +0000

    automatically backed up by learn

--- db/migrate/20171124180930_user_table.rb
+++ db/migrate/20171124180930_user_table.rb
@@ -1,7 +1,8 @@
 class UserTable < ActiveRecord::Migration
   def change
-    create_table "users", force: :cascade do |t|
+    create_table :users do |t|
       t.string :username
       t.string :password_digest
   end
+  
 end
